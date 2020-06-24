ea13230df6c4c9af00d19615db9c1b05e9f4e43c
ael-imra <ael-imra@DESKTOP-1E402L2.localdomain>
2020-06-24 02:24:23 +0100

    modified 1

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load',() => {
+    let chevronLeft = document.querySelector('.fa-chevron-left');
+    let chevronRight = document.querySelector('.fa-chevron-right');
+    let allSlide = document.getElementsByClassName('slides');
+  
+    chevronLeft.addEventListener('click',()=>{
+      for (let i = 0;i < allSlide.length;i++)
+      {
+        if (allSlide[i].getAttribute('id') === 'slideright' && allSlide[i + 1])
+        {
+          allSlide[i - 2].removeAttribute('id');
+          allSlide[i - 2].setAttribute('class', 'slides d-none');
+          allSlide[i].setAttribute('id', 'slidecenter');
+          allSlide[i - 1].setAttribute('id', 'slideleft');
+          allSlide[i + 1].setAttribute('id', 'slideright');
+          allSlide[i + 1].setAttribute('class', 'slides');
+          break;
+        }
+      }
+    });
+    chevronRight.addEventListener('click',()=>{
+      for (let i = 0;i < allSlide.length;i++)
+      {
+        if (allSlide[i].getAttribute('id') === 'slideleft' && i - 1 >= 0)
+        {
+          allSlide[i + 2].removeAttribute('id');
+          allSlide[i + 2].setAttribute('class', 'slides d-none');
+          allSlide[i].setAttribute('id', 'slidecenter');
+          allSlide[i - 1].setAttribute('id', 'slideleft');
+          allSlide[i + 1].setAttribute('id', 'slideright');
+          allSlide[i - 1].setAttribute('class', 'slides');
+          break;
+        }
+      }
+    });
+  });
